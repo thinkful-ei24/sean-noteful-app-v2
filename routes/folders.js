d484a3b023ccba7c91c4874d56e6f32422c9f707
Sean Phelan <seanphelan97@gmail.com>
2018-09-12 14:30:07 -0600

    add folder endpoints

--- routes/folders.js
+++ routes/folders.js
@@ -5,6 +5,12 @@ const knex = require('../knex');
 
 const router = express.Router();
 
+const nameError = function() {
+  const err = new Error('Missing `name` in request body');
+  err.status = 400;
+  return err;
+};
+
 router.get('/', (req, res, next) => {
   knex.select('id', 'name')
     .from('folders')
@@ -12,6 +18,71 @@ router.get('/', (req, res, next) => {
       return res.json(results);
     })
     .catch(err => next(err));
+});
+
+router.get('/:id', (req, res, next) => {
+  const {id} = req.params;
+
+  knex('folders')
+    .where({id})
+    .then(result => {
+      return res.json(result[0]);
+    })
+    .catch(err => next(err));
+});
+
+router.put('/:id', (req, res, next) => {
+  const {id} = req.params;
+  const {name} = req.body;
+
+  if(!name) {
+    const err = nameError();
+    return (err);
+  }
+
+  knex('folders')
+    .update({name})
+    .where({id})
+    .returning(['name', 'id'])
+    .then(response => {
+      return res.json(response[0]);
+    })
+    .catch(err => {
+      return next(err);
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const {name} = req.body;
+
+  if(!name) {
+    const err = nameError();
+    return next(err);
+  }
+
+  knex('folders')
+    .insert({name})
+    .returning(['name', 'id'])
+    .then(result => {
+      return res.json(result[0]);
+    })
+    .catch(err => {
+      return next(err);
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const {id} = req.params;
+
+  knex('folders')
+    .where({id})
+    .del()
+    .then(() => {
+      return res.sendStatus(204);
+    })
+    .catch(err => {
+      return next(err);
+    });
 });
 
 module.exports = router;
